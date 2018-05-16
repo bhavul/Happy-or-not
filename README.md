0b47fba183b8808d2091d3de5628bc4a94b84098
Bhavul Gauri <bhavul93@gmail.com>
2018-05-17 02:44:07 +0530

    Completed the flask app to offer predictions with UI ; updated requirements and readme

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Happy or Not?
 
-This is an ongoing work for emotion classifier. Currently, it is based on transfer learning, utilizing Inception V3 model and training last few layers over a set of images scrapped off from Instagram, for happy and non-happy (angry/sad/stressed) people. The images could contain one or more people. The model learns to about 80% validation accuracy, however is overfitting a bit right now.
+This is an ongoing work for emotion classifier. Currently, it predicts whether people in an image are happy or not. It could be one person or multiple. 
+
+It is based on transfer learning, utilizing Inception V3 model and last few layers have been trained over a set of images scrapped off from Instagram, for happy and non-happy (angry/sad/stressed) people. The images could contain one or more people. The model learns to about 80% validation accuracy, however is overfitting a bit right now.
 
 As I said, it needs to be improved and incorporate more emotions. 
 
-The jupyter notebook walks through each step with documentation. The model(h5) file has not been attached to git because it was 200+ MB. 
+### How to use?
+
+1. Install the dependencies (`pip install -r requirements.txt`)
+2. Open the jupyter notebook inside experiments section, and run all the cells. On my macbook pro the training took about 45-50 minutes. So, have a coffee while it trains the first model.
+3. Run the flask app (`python app.py`) and navigate to localhost:5000 in your browser. 
+4. Select the model from dropdown, and try inserting an image URL which contains happy / sad person(s) and click on 'Predict'.
+
+The page would reload in few moments and show you both your image and project's prediction for it with confidence. 
+
+### Project Structure
+The jupyter notebook inside `experiments` folder walks through each step with documentation to reach the model. 
+
+The model(h5) file has not been attached to git because it was 200+ MB. However, all models go inside `models` directory. These are keras models that should be saved using `model.save('some_model_name.h5')`. This meanst each model should be having its weights, architecture, loss and state of optimization stored in it.
+ 
+ The `templates` folder contains the jinja2 template which the flask app uses to showcase the html. 
+ 
+ Lastly, `app.py` is the flask front-end to test this project out! Remember, you need to go through the notebook once to first generate a model. 
--- app.py
+++ app.py
@@ -1,22 +1,80 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import requests
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+import os
+from os import listdir
+import cv2
+import tensorflow as tf
 
 app = Flask(__name__)
+models_dict = {}
+graph = None
 
 # todo
-# fetch the requested model value from models directory and then
+# update readme.
+
+def get_root_directory_of_this_app():
+    return os.path.dirname(os.path.abspath(__file__))
+
+def get_image_in_np_format(image_url):
+    image_np = np.array(Image.open(requests.get(image_url, stream=True).raw))
+    im = cv2.resize(image_np, (299, 299), interpolation=cv2.INTER_CUBIC)
+    return im
+
+def load_keras_models():
+    global graph
+    global models_dict
+    graph = tf.get_default_graph()
+    models = get_available_models()
+    for modelname in models:
+        model_file_path = get_root_directory_of_this_app()+"/models/"+modelname+".h5"
+        model = load_model(model_file_path)
+        models_dict[modelname] = model
+    return models_dict
+
+
+def predict_happiness(image_np, model_name):
+    image = image_np.reshape((1,image_np.shape[0],image_np.shape[1],image_np.shape[2]))
+    model = models_dict[model_name]
+    with graph.as_default():
+        prediction = model.predict(image)
+    state = None
+    confidence = None
+    if(prediction[0][0] > prediction[0][1]):
+        state = 'Happy'
+        confidence = prediction[0][0]
+    else:
+        state = 'Not Happy'
+        confidence = prediction[0][1]
+    return state, confidence
+
+
+def get_available_models():
+    current_directory = get_root_directory_of_this_app()
+    return [filename.split('.')[0] for filename in listdir(current_directory+"/models") if filename.endswith('.h5')]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def do_prediction():
-    # todo - replace with model stuff
-    models = ['model1','model2']
-    image_url = 'http://i39.tinypic.com/anzl1k.jpg'
-    prediction = 'happy!'
-    confidence = '0.09'
-
+    models = get_available_models()
     if request.method == 'POST':
+        model_name = request.form['modelName']
+        image_url = request.form['imageUrl']
+        image_np = get_image_in_np_format(image_url)
+        prediction, confidence = predict_happiness(image_np, model_name)
         return render_template('index.html',models=models,show_prediction=True,imageUrl=image_url,
                                prediction=prediction, confidence=confidence)
     else:
         return render_template('index.html',models=models,show_prediction=False,)
+
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    print(("* Loading Keras model and Flask starting server..."
+           "please wait until server has fully started"))
+    load_keras_models()
+    app.run(port=5000, debug=False)
